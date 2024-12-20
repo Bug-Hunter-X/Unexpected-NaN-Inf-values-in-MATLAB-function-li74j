d3894a2bdfbdb8b5eeb00bcc2e617a8f3f1edd21
BugHunter-X <askzenai@gmail.com>
2024-12-20 13:46:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,12 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  if input < 0
+    error('Input must be non-negative');
+  end
+  result = someCalculation(input);
+end
+
+function output = someCalculation(x)
+  % Some other calculation that could produce unexpected NaN or Inf values
+  output = 1/x;
+end
